ae7c49cb5ef40899113fa884f7f83ef2cb734194
timausk <timausk@googlemail.com>
2020-09-16 13:53:06 +0200

    Add ui component inputField

---
+++ src/components/ui/form/InputField.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let name;
+  export let type = 'text';
+  export let value;
+  export let id;
+  export let label;
+  export let onChangeHandler = () => {};
+
+  const setLabelText = () => {
+    return label || value;
+  };
+
+</script>
+<label for="{id}">
+  <input
+    {name} {type} {value} {id}
+    on:change={onChangeHandler} on:blure={onChangeHandler}
+  >
+  {setLabelText()}
+</label>
